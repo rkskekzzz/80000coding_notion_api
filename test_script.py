07dadf82d64dd6ce17fe0af9cc214e755d94134a
rkskekzzz <wkdlfflxh@naver.com>
2022-04-26 00:58:56 +0900

    Feat : 컨트리뷰터 바로 참조, 100개이상 반복문 사용

--- test_script.py
+++ test_script.py
@@ -4,11 +4,12 @@ import requests
 import json
 import os
 import argparse
+import pandas as pd
 from dotenv import load_dotenv
 
 parser = argparse.ArgumentParser(description='팔만코딩경 컨트리뷰터 활동 체크 스크립트')
-parser.add_argument('--date', required=True, help='측정을 시작할 날짜')
-parser.add_argument('--contributors', required=True, help='컨트리뷰터 리스트')
+parser.add_argument('--start-date', '-s', required=True, help='측정을 시작할 날짜')
+# parser.add_argument('--end-date', '-e', required=True, help='측정이 끝나야할 날짜')
 
 args = parser.parse_args()
 
@@ -20,8 +21,10 @@ args = parser.parse_args()
 load_dotenv()
 my_bearer = os.getenv("BEARER")
 
-# notion api url
-url = "https://api.notion.com/v1/databases/94e689f5b19947b1ab0f9b5f2d52962b/query"
+# notion api libraryDbUrl
+libraryDbUrl = "https://api.notion.com/v1/databases/94e689f5b19947b1ab0f9b5f2d52962b/query"
+contributorDbUrl = "https://api.notion.com/v1/databases/10909fc406ab42c29fa7f82d6a08d741/query"
+
 headers = {
     "Authorization": "Bearer {}".format(my_bearer),
     "Accept": "application/json",
@@ -29,13 +32,13 @@ headers = {
     "Content-Type": "application/json"
 }
 
-body = {
+libraryBody = {
     "filter": {
         "and": [
             {
                 "property": "create_date",
                 "created_time": {
-                    "on_or_after": "{}".format(args.date),
+                    "on_or_after": "{}".format(args.start_date),
                 }
             },
             {
@@ -49,16 +52,46 @@ body = {
     "page_size": 100
 }
 
-response = requests.request("POST", url, headers=headers, data=json.dumps(body))
-jsonObject = json.loads(response.text)
+contributorBody = {
+    "filter": {
+        "property": "구분",
+        "multi_select" : {
+            "contains": "2기"
+        }
+    }
+}
+
+
+libraryObjectList = []
+while True:
+    libraryResponse = requests.request("POST", libraryDbUrl, headers=headers, data=json.dumps(libraryBody))
+    libraryObject = json.loads(libraryResponse.text)
+    libraryObjectList.extend(libraryObject["results"])
+    if not libraryObject["next_cursor"]:
+        break
+    libraryBody["start_cursor"] = libraryObject["next_cursor"]
+
+print(len(libraryObjectList))
+contributorResponse = requests.request("POST", contributorDbUrl, headers=headers, data=json.dumps(contributorBody))
+contributorObject = json.loads(contributorResponse.text)
+
+contributorList = []
+for contributor in contributorObject["results"]:
+    contributorList.append(contributor["properties"]["이름"]['title'][0]["plain_text"])
 
 def filterByContributors(x, cadet):
-    return cadet == x["properties"]["cadet_intraID"]["rich_text"][0]["plain_text"]
+    return cadet == x["properties"]["rich_text"]["formula"]["string"]
 
 
-for contributor in args.contributors.split():
-    result = [x for x in jsonObject["results"] if filterByContributors(x, contributor)]
-    print("{} : {}개".format(contributor, len(result)))
+resultList = []
+with open("result.txt", 'w') as resultFile:
+    for contributor in contributorList:
+        result = [x for x in libraryObjectList if filterByContributors(x, contributor)]
+        resultList.append([contributor, str(len(result))])
+
+
+
+
 
 
 
